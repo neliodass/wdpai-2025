8b90ccc8acb377dab0a8c4bb62d34ac5da005a3e
Bartosz Pieróg <pierogbartoszzstti@gmail.com>
2025-11-03 13:00:58 +0100

    add header layout and update login, register, and dashboard pages

---
+++ public/views/layouts/header.php
@@ -0,0 +1,8 @@
+<!-- app/views/layouts/header.php -->
+<head>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap" rel="stylesheet">
+
+</head>
